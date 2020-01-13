ad90616704ee7cfb15d525b079d8d8400cd07bd5
charles.corbett@wurl.com <nafredy@gmail.com>
2020-01-13 14:13:50 -0800

    new ETL flow appears to be working

--- highwinds/resource_configuration_helpers.go
+++ highwinds/resource_configuration_helpers.go
@@ -27,113 +27,19 @@ func buildConfigurationFromState(d *schema.ResourceData) (*models.Configuration,
 
 	if v, ok := d.GetOk("scope"); ok {
 		config.Scope = models.StructFromMap(&models.Scope{}, v.(map[string]interface{})).(*models.Scope)
+		//return nil, fmt.Errorf("Erroring on purpose: %s", "scope")
 	}
 
 	if v, ok := d.GetOk("hostnames"); ok {
 		config.Hostname = models.ScopeHostnameFromInterfaceSlice(v.([]interface{}))
 	}
 
-	// weighted set
-	if v, ok := d.GetOk("cache_policy"); ok {
-		models, err := expandOriginPullPolicies(getSliceIfaceFromSet(v))
-		if err != nil {
-			return nil, err
-		}
-		config.OriginPullPolicy = models
-	}
-
-	if v, ok := d.GetOk("stale_cache_extension"); ok {
-		config.OriginPullCacheExtension = models.StructFromMap(&models.OriginPullCacheExtension{}, v.(*schema.Set).List()[0].(map[string]interface{})).(*models.OriginPullCacheExtension)
-	}
-
-	// weighted set
-	if v, ok := d.GetOk("origin_request_edge_rule"); ok {
-		models, err := expandOriginRequestModification(getSliceIfaceFromSet(v))
-		if err != nil {
-			return nil, err
-		}
-		config.OriginRequestModification = models
-	}
-
-	// weighted set
-	if v, ok := d.GetOk("origin_response_edge_rule"); ok {
-		models, err := expandOriginResponseModification(getSliceIfaceFromSet(v))
-		if err != nil {
-			return nil, err
-		}
-		config.OriginResponseModification = models
-	}
-
-	// weighted set
-	if v, ok := d.GetOk("client_request_edge_rule"); ok {
-		models, err := expandClientRequestModification(getSliceIfaceFromSet(v))
-		if err != nil {
-			return nil, err
-		}
-		config.ClientRequestModification = models
-	}
-
-	// weighted set
-	if v, ok := d.GetOk("client_response_edge_rule"); ok {
-		models, err := expandClientResponseModification(getSliceIfaceFromSet(v))
-		if err != nil {
-			return nil, err
-		}
-		config.ClientResponseModification = models
-	}
-
-	// Delivery is a complex set
 	if v, ok := d.GetOk("delivery"); ok {
-		delivery := expandSetOfMaps(v)
-
-		err := config.ExpandParentFields(delivery)
-		if err != nil {
-			return nil, err
-		}
-
-		/*
-			debug.Log("DEBUG CHECK", "SETTING MODEL")
-
-			if compression, ok := delivery["compression"]; ok {
-				config.Compression = models.StructFromMap(&models.Compression{}, getMapFromZeroedSet(compression)).(*models.Compression)
-			}
-
-			if staticHeader, ok := delivery["static_header"]; ok {
-				sh, err := expandDeliveryStaticHeader(getSliceIfaceFromSet(staticHeader))
-				if err != nil {
-					return nil, err
-				}
-				config.StaticHeader = sh
-			}
-
-			if httpMethods, ok := delivery["http_methods"]; ok {
-				config.HTTPMethods = models.StructFromMap(&models.HTTPMethods{}, getMapFromZeroedSet(httpMethods)).(*models.HTTPMethods)
-			}
-
-			if responseHeader, ok := delivery["response_header"]; ok {
-				config.ResponseHeader = models.StructFromMap(&models.ResponseHeader{}, getMapFromZeroedSet(responseHeader)).(*models.ResponseHeader)
-			}
-				debug.Log("DEBUG CHECK", "bandwidth_rate_limiting MODEL")
-
-				if bwRL, ok := delivery["bandwidth_rate_limiting"]; ok {
-					config.BandwidthRateLimit = models.StructFromMap(&models.BandwidthRateLimit{}, getMapFromZeroedSet(bwRL)).(*models.BandwidthRateLimit)
-				}
-
-				debug.Log("DEBUG CHECK", "pattern_based_rate_limiting MODEL")
-
-				if bwL, ok := delivery["pattern_based_rate_limiting"]; ok {
-					config.BandwidthLimit = models.StructFromMap(&models.BandwidthLimit{}, getMapFromZeroedSet(bwL)).(*models.BandwidthLimit)
-				}
-		*/
+		config.IngestSchema(expandSetOfMaps(v))
 	}
 
-	// Origin is a complex set
 	if v, ok := d.GetOk("origin"); ok {
-		origin := expandSetOfMaps(v)
-
-		if originPullHost, ok := origin["origin_pull_host"]; ok {
-			config.OriginPullHost = models.StructFromMap(&models.OriginPullHost{}, getMapFromZeroedSet(originPullHost)).(*models.OriginPullHost)
-		}
+		config.IngestSchema(expandSetOfMaps(v))
 	}
 
 	debug.Log("STATE", "%v", spew.Sprintf("%v", config.Scope))
@@ -144,8 +50,6 @@ func buildConfigurationFromState(d *schema.ResourceData) (*models.Configuration,
 // ingestState updates terraform state from a configuration model
 func ingestState(d *schema.ResourceData, config *models.Configuration) []error {
 	var errs []error
-
-	//parents := config.CompressParentFields()[0].(map[string]map[string]interface{})
 
 	// Set scope details
 	err := d.Set("scope", models.MapFromStruct(config.Scope))
@@ -159,54 +63,21 @@ func ingestState(d *schema.ResourceData, config *models.Configuration) []error {
 		errs = append(errs, err)
 	}
 
-	// Delivery is a complex set
-	//err = d.Set("delivery", []interface{}{parents["delivery"]})
-	//if err != nil {
-	//	errs = append(errs, err)
-	//}
+	// Should only be used for schema.Set's of schema.Sets
+	content := config.ExtractSchema() //[0].(map[string]interface{})
 
-	// Origin is a complex set
-	//err = d.Set("origin", compressOriginSet(config))
-	//err = d.Set("origin", []interface{}{parents["origin"]})
-	//if err != nil {
-	//	errs = append(errs, err)
-	//}
+	debug.Log("DELIVERY", "%v", spew.Sprintf("%v", content["delivery"]))
 
-	// Set stale_cache_extension - unweighted set
-	err = d.Set("stale_cache_extension", []interface{}{models.MapFromStruct(config.OriginPullCacheExtension)})
+	// TODO: returned as map not interface, take a look if this is intended
+	err = d.Set("delivery", []interface{}{content["delivery"]})
 	if err != nil {
 		errs = append(errs, err)
 	}
 
-	// Set cache_policy (OriginPullPolicy) - weighted
-	err = d.Set("cache_policy", compressOriginPullPolicies(config.OriginPullPolicy))
+	err = d.Set("origin", []interface{}{content["origin"]})
 	if err != nil {
 		errs = append(errs, err)
 	}
-
-	// Set Edge Rules
-	err = d.Set("origin_request_edge_rule", compressOriginRequestModification(config.OriginRequestModification))
-	if err != nil {
-		errs = append(errs, err)
-	}
-
-	err = d.Set("origin_response_edge_rule", compressOriginResponseModification(config.OriginResponseModification))
-	if err != nil {
-		errs = append(errs, err)
-	}
-
-	err = d.Set("client_request_edge_rule", compressClientRequestModification(config.ClientRequestModification))
-	if err != nil {
-		errs = append(errs, err)
-	}
-
-	err = d.Set("client_response_edge_rule", compressClientResponseModification(config.ClientResponseModification))
-	if err != nil {
-		errs = append(errs, err)
-	}
-
-	test := d.Get("delivery")
-	debug.Log("FJDSKLFLSDKFLKSFKLSDF", "%v", spew.Sprintf("%v", test))
 
 	return errs
 }
